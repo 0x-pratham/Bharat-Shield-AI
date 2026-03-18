436209df265c9ad6b02ae884f18c03e11cd7f7af
Akash Lad <ladakash2003@gmail.com>
2026-03-18 14:36:06 +0530

    Upgrade little with dadtaset with malware probality and risk Score

--- ai_model/train_model.py
+++ ai_model/train_model.py
@@ -5,7 +5,7 @@ import joblib
 # Load dataset
 data = pd.read_csv("dataset/malware_dataset.csv")
 
-# Features and labels
+# Features and label
 X = data.drop("malware", axis=1)
 y = data["malware"]
 
@@ -14,10 +14,26 @@ model = RandomForestClassifier()
 model.fit(X, y)
 
 # Test prediction
-test_data = [[12,3,1,2,1,4]]
-prediction = model.predict(test_data)
+sample = pd.DataFrame([[12,1,1,2,1,1]], columns=X.columns)
+
+prediction = model.predict(sample)
+probability = model.predict_proba(sample)
 
 print("Prediction:", prediction)
+print("Malware Probability:", probability[0][1])
+
+# Convert to risk score
+risk_score = int(probability[0][1] * 100)
+
+if risk_score > 70:
+    status = "Dangerous"
+elif risk_score > 40:
+    status = "Suspicious"
+else:
+    status = "Safe"
+
+print("Risk Score:", risk_score)
+print("Status:", status)
 
 # Save model
 joblib.dump(model, "model/bharatshield_model.pkl")
